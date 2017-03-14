e5748d8d68bff5baf3d3b48da5912e92b3668fc1
mhirna <hirnamarija@gmail.com>
2017-03-14 23:56:03 +0200

    test added

--- all_prefixes.py
+++ all_prefixes.py
@@ -2,3 +2,4 @@ def all_prefixes(s):
     if type(s) == str:
         return set([s[:i] for i in range(1, len(s) + 1)])
     return None
+print(all_prefixes('lol'))
--- test_prefixes.py
+++ test_prefixes.py
@@ -4,4 +4,5 @@ from all_prefixes import all_prefixes
 def test_type():
     assert all_prefixes(3) == None
 
-def test_
+def test_all_prefixes():
+    assert all_prefixes('lol') == {'l', 'lol', 'lo'}
