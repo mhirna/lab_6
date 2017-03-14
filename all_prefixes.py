0b752b1c0b8016d0f6213eefd7c38066fa5a9e84
mhirna <hirnamarija@gmail.com>
2017-03-15 00:08:59 +0200

    all_prefixes changed

--- all_prefixes.py
+++ all_prefixes.py
@@ -1,5 +1,9 @@
 def all_prefixes(s):
     if type(s) == str:
-        return set([s[:i] for i in range(1, len(s) + 1)])
+        set_str = set()
+        s_index = [i for i in range(len(s)) if s[i] == s[0]]
+        for ind in s_index:
+            set_str = set_str | set([s[ind:i] for i in range(ind + 1, len(s) + 1)])
+        return set_str
     return None
-print(all_prefixes('lol'))
+print(all_prefixes('pepsi'))
--- test_prefixes.py
+++ test_prefixes.py
@@ -4,5 +4,12 @@ from all_prefixes import all_prefixes
 def test_type():
     assert all_prefixes(3) == None
 
+
 def test_all_prefixes():
-    assert all_prefixes('lol') == {'l', 'lol', 'lo'}
+    assert all_prefixes('lock') == {'l', 'loc', 'lo', 'lock'}
+
+
+def test_mult_let():
+    assert all_prefixes('pepsi') == {'pep', 'peps', 'pepsi', 'p', 'pe', 'psi', 'ps'}
+
+
